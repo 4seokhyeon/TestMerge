7d5afae7b0f95b0c198cae2d40c86976f4b137ee
4seokhyeon <106301222+4seokhyeon@users.noreply.github.com>
2025-01-14 15:33:21 +0900

    Test Commit 2

---
+++ app/src/main/java/kr/co/nod/myapplication/MainActivity2.kt
@@ -0,0 +1,24 @@
+package kr.co.nod.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * Loads [MainFragment].
+ */
+class MainActivity2 : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_browse_fragment, MainFragment())
+                .commitNow()
+        }
+    }
+}
+
+private fun FragmentTransaction.replace(mainBrowseFragment: Int, mainFragment: MainFragment): FragmentTransaction {
+
+}
